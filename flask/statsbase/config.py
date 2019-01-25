c457ef923347f462e4e8b94f00468bcf44f532d9
Jonathan Shek <jonathanJJShek@gmail.com>
2019-01-25 11:22:49 -0800

    testing again

--- flask/statsbase/config.py
+++ flask/statsbase/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://prod:242amherst@23.226.138.66/prod'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
