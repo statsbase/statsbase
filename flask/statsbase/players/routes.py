5f185a4ce6c442055c8e43a8e3de8ab9b4763c32
Jonathan Shek <jonathanJJShek@gmail.com>
2019-01-28 11:29:04 -0800

    removed bokeh folder, included graphs folders in flask/statsbase/templates

--- flask/statsbase/players/routes.py
+++ flask/statsbase/players/routes.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
-players = Blueprint('players', __name__)
+players = Blueprint('players', __name__, template_folder='statsbase/bokeh/templates')
 
 @players.route("/players/guards/fta", methods=['GET'])
 def guardsfta():
-	return render_template('guardsfta.html')
+	return render_template('players/guardsfta.html')
 
 
 
--- flask/statsbase/teams/routes.py
+++ flask/statsbase/teams/routes.py
@@ -7,7 +7,7 @@ teams = Blueprint('teams', __name__)
 
 @teams.route("/teams/fta", methods=['GET'])
 def teamfta():
-	return render_template('teamfta.html')
+	return render_template('teams/teamfta.html')
 
 
 
